ad63f7fb2ba5949b79e0b7a148a417aff06b6998
Akash K. <23f1001065@ds.study.iitm.ac.in>
2025-06-21 20:24:52 +0530

    Create create_json.py

---
+++ create_json.py
@@ -0,0 +1,9 @@
+import json 
+from datetime import datetime, UTC
+def create_file():
+  now = datetime.now(UTC)
+  with open(f'demo-file-{now.strftime("%Y-%m-%d")}.json') as f:
+    f.write(json.dumps({"timestamp": now.isoformat(), "complete_time": now} + "\n")
+
+if __name__=="__main__":
+  create_file()
